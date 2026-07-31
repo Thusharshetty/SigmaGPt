b114029bb113ddca3d9ee46bd1eb0ddaf86c69e2
Thushar shetty <shettythushar31@gmail.com>
2026-07-31 21:46:01 +0530

    chatwindow-2

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,19 +1,27 @@
 import React, { useContext } from 'react'
 import './Chat.css'
 import { MyContext } from './MyContext'
+import ReactMarkdown from 'react-markdown';
+import rehypehighlight from 'rehype-highlight'
+import "highlight.js/styles/github.css"
 
 function Chat() {
   const {newChat,chats}=useContext(MyContext)
   return (
     <>
       {newChat && <h1>Start a new chat!</h1>}
+      
       <div className="chats">
-        <div className="userDiv">
-          <p className="userMessage">user Chat</p>
-        </div>
-        <div className="gptDiv">
-          <p className="gptMessage">Gpt Chat</p>
-        </div>
+        {
+          chats?.map((chat,idx)=>(
+            <div className={chat.role === "user" ? "userDiv" : "gptDiv"} key={idx}>
+              {chat.role === "user"  ?
+               <p className='userMessage'>{chat.content}</p>
+               :<ReactMarkdown rehypePlugins={[rehypehighlight]}>{chat.content}</ReactMarkdown>
+               }
+            </div>
+          ))
+        }
       </div>
     </>
   )
