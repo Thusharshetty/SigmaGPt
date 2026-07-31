cbce4035f5baa2a86eb75807a270221cb214c766
Thushar shetty <shettythushar31@gmail.com>
2026-07-31 13:55:15 +0530

    chatwindow

--- client/src/ChatWindow.jsx
+++ client/src/ChatWindow.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import './ChatWindow.css'
+import Chat from './Chat.jsx'
 
 
 function ChatWindow() {
   return (
-    <>
-      <h1>ChatWindow</h1>
-    </>
+    <div className='chatwindow'>
+      <div className='navbar'>
+        <span>SigmaGpt <i className="fa-solid fa-chevron-down"></i></span>
+        <div className="userIcondiv">
+          <span className='userIcon'><i className="fa-solid fa-user"></i></span>
+        </div>
+      </div>
+      <Chat></Chat>
+      <div className="chatInput">
+        <div className="inputBox">
+          <input type="text" name="" id="" placeholder='Ask anything'/>
+          <div id="submit"><i className="fa-solid fa-paper-plane"></i></div>
+        </div>
+        <p className="info">
+          SigmaGpt can make mistakes. Check important info. See cookie preferences
+        </p>
+      </div>
+    </div>
   )
 }
 
