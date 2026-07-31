5178a1f13c6fcf5b890e877272cdd8b935f26014
Thushar shetty <shettythushar31@gmail.com>
2026-07-31 11:41:55 +0530

    sideBar

--- client/src/SideBar.jsx
+++ client/src/SideBar.jsx
@@ -3,9 +3,21 @@ import './SideBar.css';
 
 function SideBar() {
   return (
-    <>
-      <h1>SideBar</h1>
-    </>
+    <section className='sidebar'>
+      <button>
+        <img src="src/assets/blacklogo.png" alt="gpt-logo"  className='logo'/>
+       <span> <i className="fa-solid fa-pen-to-square"></i></span>
+      </button>
+      <ul className='history'>
+        <li>history1</li>
+        <li>history2</li>
+        <li>history3</li>
+      </ul>
+
+      <div className='sign'>
+        <p>By Apna college &hearts;</p>
+      </div>
+    </section>
   )
 }
 
